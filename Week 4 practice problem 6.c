5f2acc1c933a94efb46048b126a85d3f766b30f5
paul-partho <10legendgaming@gmail.com>
2025-07-20 23:29:38 +0600

    Week 4 practice problem 6.c

--- Week 4 practice problem 6.c
+++ Week 4 practice problem 6.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    int s=1;
-    printf("Enter any number: ");
-    scanf("%d",&n);
-    printf("1");
-    for(i=2;i<=n;i=i+1)
-    {
-        s=s+i*(i*2)*(i*3);
-        printf(" + %d*%d*%d",i,i*2,i*3);
-    }
-    printf(" = %d",s);
-    return 0;
+   int i,n,s=1;
+   scanf("%d",&n);
+   printf("1+");
+   for(i=2;i<=n;i=i+1)
+   {
+       s=s+i*i*i*6;
+       if(i<n)
+       {
+           printf("%d*%d*%d+",i,i*2,i*3);
+       }
+       else
+       {
+           printf("%d*%d*%d=",i,i*2,i*3);
+       }
+   }
+   printf("%d",s);
 }
